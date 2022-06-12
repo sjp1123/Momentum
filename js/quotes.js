3fcc4e72b5548836ec64387c7ae9981cc7f90e8e
Shijun Park <shijun2009@naver.com>
2022-06-12 18:05:16 +0900

    final

--- js/quotes.js
+++ js/quotes.js
@@ -158,8 +158,8 @@ const quotes = [
     },
 ];
 
-const quote = document.querySelector("#quote a span");
-const author = document.querySelector("#quote > span");
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
-function onGeoOk(position) { 
+const API_KEY = "898ddf8427da1d1ac25a6cbf10b3a48b";
+
+function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const temp = document.querySelector("#weather span:last-child");
+            weather.innerText = "weather : " + data.weather[0].main;
+            temp.innerText = "temperature : " + data.main.temp + "°C";
+        });
 }
 
 function onGeoError() {
